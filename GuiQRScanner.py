2d0b5c13621a83c7625e3e12af1a42d9b75f2be1
ruben <rubenski0345@gmail.com>
2020-06-24 14:29:27 +0200

    Documentation added and short instructions button

--- GuiQRScanner.py
+++ GuiQRScanner.py
@@ -1,11 +1,10 @@
-from tkinter import *
-from tkinter.ttk import Combobox
-from webcolors import *
-import json
+from tkinter import * #tkinter Library
+from tkinter.ttk import Combobox #combobox from tkinter Library
+from webcolors import * #Html colors to decimal colors Library
+import json #use of json files Library
+import QrRecognizingCombi as fl #Able to make use of other file
 
-import QrRecognizingCombi as fl
-
-root = Tk()
+root = Tk() #start of tkinter
 ###########################################
 #https://datatofish.com/entry-box-tkinter/
 #https://stackoverflow.com/questions/28905984/tkinter-how-to-create-choice-box
@@ -14,26 +13,26 @@ root = Tk()
 #https://www.youtube.com/watch?v=lt78_05hHSk
 #https://stackoverflow.com/questions/2395431/using-tkinter-in-python-to-edit-the-title-bar
 #https://thispointer.com/python-how-to-check-if-an-item-exists-in-list-search-by-value-or-condition/
-#
-#
+#https://datatofish.com/insert-sql-server-python/
+#https://stackoverflow.com/questions/4253960/sql-how-to-properly-check-if-a-record-exists
 ######################################
-root.title('Barcode Scanner')
-root.iconbitmap('TkinterIcon.ico')
-FirstSection = Label(root, text="Camera Options", fg="blue")
+root.title('Barcode Scanner') #title
+root.iconbitmap('TkinterIcon.ico') #title Picture
+FirstSection = Label(root, text="Camera Options", fg="blue") #first title
 FirstSection.grid(row=0)
-SecondSection = Label(root, text="Recognize Square Options", fg="blue")
+SecondSection = Label(root, text="Recognize Square Options", fg="blue") #second title
 SecondSection.grid(row=4)
-ThirdSection = Label(root, text="ErrorBox", fg="blue")
+ThirdSection = Label(root, text="ErrorBox", fg="blue") #third title
 ThirdSection.grid(row=4, column=3)
-FourthSection = Label(root, text="DataBase Printing (optional)", fg="blue")
+FourthSection = Label(root, text="DataBase Printing (optional)", fg="blue")#fourth title
 FourthSection.grid(row=8)
 #####################################
 def get_ips():
-    IPs = []
-    IPfile_Opener = open('UsedIPs.json')
-    IPData = json.load(IPfile_Opener)
-    for IP in IPData:
-        IPs.append(IP)
+    IPs = [] #create empty list
+    IPfile_Opener = open('UsedIPs.json') #opens json file
+    IPData = json.load(IPfile_Opener) #loads all data
+    for IP in IPData: #seperate data with for loop
+        IPs.append(IP) #adds it to empty list
     return IPs
 
 def get_databases():
@@ -44,18 +43,20 @@ def get_databases():
         Databases.append(Database)
     return Databases
 
-######################################
-Colors = ['RED', "BLUE", "GREEN", "BLACK", "YELLOW"]
-live_camera_label = Label(root, text="Camera Nummer/IP")
+###################################### First Section
+Colors = ['RED', "BLUE", "GREEN", "BLACK", "YELLOW"] #frame colors list
+live_camera_label = Label(root, text="Camera Nummer/IP") #Makes Text Box
 live_usedIps_label = Label(root, text="Early Used Ips")
 live_framewidth_label= Label(root, text="Frame Width")
 live_frameheight_label= Label(root, text="Frame Height")
-IPs = get_ips()
-live_camera_entry = Entry(root)
-live_usedIps_entry = Combobox(root, values = IPs)
+IPs = get_ips() #get Ips from IP function
+
+live_camera_entry = Entry(root) #makes Entry box
+live_usedIps_entry = Combobox(root, values = IPs) #Makes Combo Box
 live_framewidth_entry = Entry(root)
 live_frameheight_entry = Entry(root)
-live_camera_label.grid(row=1)
+
+live_camera_label.grid(row=1) #.grid == inserts to tkinter GUI and where
 live_usedIps_label.grid(row=1, column=3)
 live_framewidth_label.grid(row=2)
 live_frameheight_label.grid(row=3)
@@ -64,44 +65,54 @@ live_usedIps_entry.grid(row=1, column=4)
 live_framewidth_entry.grid(row=2, column=1)
 live_frameheight_entry.grid(row=3, column=1)
 
-######################################
+###################################### Second Section
 live_objectname_label = Label(root, text="Object Name")
 live_color_label = Label(root, text="Frame Color")
+
 live_objectname_entry = Entry(root)
 live_colorMenu = Combobox(root, values = Colors)
+
 live_objectname_label.grid(row=5)
 live_color_label.grid(row=6)
 live_objectname_entry.grid(row=5, column=1)
 live_colorMenu.grid(row=6, column=1)
-#######################################
+
+####################################### Third Section
 live_DBdriver_label = Label(root, text="Driver:")
 live_DBdriver_label.grid(row=9)
 live_DBdriver_entry = Entry(root)
 live_DBdriver_entry.grid(row=9, column=1)
+
 live_DBserver_label = Label(root, text="Server:")
 live_DBserver_label.grid(row=10)
 live_DBserver_entry = Entry(root)
 live_DBserver_entry.grid(row=10, column=1)
+
 live_DBdatabase_label = Label(root, text="Database:")
 live_DBdatabase_label.grid(row=11)
 live_DBdatabase_entry = Entry(root)
 live_DBdatabase_entry.grid(row=11, column=1)
+
 live_DBuid_label = Label(root, text="UID:")
 live_DBuid_label.grid(row=12)
 live_DBuid_entry = Entry(root)
 live_DBuid_entry.grid(row=12, column=1)
+
 live_DBpwd_label = Label(root, text="PWD:")
 live_DBpwd_label.grid(row=13)
 live_DBpwd_entry = Entry(root)
 live_DBpwd_entry.grid(row=13, column=1)
+
 live_DBselect_label = Label(root, text="SELECT:")
 live_DBselect_label.grid(row=14)
 live_DBselect_entry = Entry(root)
 live_DBselect_entry.grid(row=14, column=1)
+
 live_DBfrom_label = Label(root, text="FROM:")
 live_DBfrom_label.grid(row=15)
 live_DBfrom_entry = Entry(root)
 live_DBfrom_entry.grid(row=15, column=1)
+
 live_DBsection_label = Label(root, text="SECTION:")
 live_DBsection_label.grid(row=16)
 live_DBsection_entry = Entry(root)
@@ -116,37 +127,38 @@ live_DBsection_entry.grid(row=16, column=1)
 #~
 ######################################
 
-
 def RecognizingStartFile():
     connectie = ''
     execute = ''
     insert = ''
-    databaseUse = False
-    frameWidth, frameHeight, path, camnum, objectName, color = fl.defaultSettings()
+    databaseUse = False #default is not used
+    CameraError = False #default if not changed
+    frameWidth, frameHeight, path, camnum, objectName, color = fl.defaultSettings() #if not inserted, this data will be used
     try:
-        if live_objectname_entry.get() != '':
-            objectName = live_objectname_entry.get()
+        if live_objectname_entry.get() != '': #if entrybox is filled
+            objectName = live_objectname_entry.get() #use the filled text
         if live_framewidth_entry.get() != '':
             frameWidth = int(live_framewidth_entry.get())
         if live_frameheight_entry.get()!= '':
             frameHeight = int(live_frameheight_entry.get())
         if live_colorMenu.get() != '':
             color = name_to_rgb(live_colorMenu.get())
-        if live_usedIps_entry.get() != '':
-            if len(live_usedIps_entry.get()) > 4:
-                camnum = 'rtsp://root:TO-41212@' + live_usedIps_entry.get() + '/live.sdp'
-            else:
-                camnum = int(live_usedIps_entry.get())
-        elif live_camera_entry.get() != '':
+        if live_usedIps_entry.get() != '':  #if using formerly used IPS
+            if len(live_usedIps_entry.get()) > 4: #if long enough to be IP
+                camnum = 'rtsp://root:TO-41212@' + live_usedIps_entry.get() + '/live.sdp' #make useable IP command
+            else: #if Camera Number
+                camnum = int(live_usedIps_entry.get()) #use only the number
+        elif live_camera_entry.get() != '': #else use Manual IP filled text
             if len(live_camera_entry.get()) > 4:
                 camnum = 'rtsp://root:TO-41212@' + live_camera_entry.get() + '/live.sdp'
-                if live_camera_entry.get() not in IPs:
-                    IPs.append(live_camera_entry.get())
-                with open('UsedIPs.json', 'w') as IP_file:
-                    json.dump(IPs, IP_file, indent=4)
+                if live_camera_entry.get() not in IPs: #check if IP already in history
+                    IPs.append(live_camera_entry.get()) #append it to list
+                with open('UsedIPs.json', 'w') as IP_file: #open the json file
+                    json.dump(IPs, IP_file, indent=4) #dump the IP list in Json file
             else:
                 camnum = int(live_camera_entry.get())
         if live_DBdriver_entry.get() != '' and live_DBserver_entry.get() != '' and live_DBuid_entry.get() != '' and live_DBpwd_entry.get() != '' and live_DBselect_entry.get() != '' and live_DBfrom_entry.get() != '' and live_DBsection_entry.get() != '' and live_DBdatabase_entry.get() != '' :
+            #check if all the database insert slots are filled
             databaseUse = True
             connectie = 'Driver=' + live_DBdriver_entry.get() + ';'+'Server=' + live_DBserver_entry.get() + ';'+'Database=' + live_DBdatabase_entry.get() +';' +'UID=' + live_DBuid_entry.get() + ';' + 'PWD=' + live_DBpwd_entry.get() + ';'
             execute = 'SELECT' + live_DBselect_entry.get() + 'FROM' + live_DBfrom_entry.get()
@@ -154,13 +166,15 @@ def RecognizingStartFile():
             connectielist = [connectie, execute, insert]
             with open('UsedDatabases.json', 'w') as IP_file:
                 json.dump(IPs, IP_file, indent=4)
-    except ValueError:
-        CameraError = False
+        else:
+            print("database not used or filled entirely")
+    except ValueError: #if not filled in correctly
         ErrorEntrys(CameraError)
 
     fl.cascadeRunning(frameWidth, frameHeight, path, camnum, objectName, color, databaseUse, connectie, execute, insert)
+    #run with the current made settings
 
-def ErrorEntrys(CameraError):
+def ErrorEntrys(CameraError): #ErrorBox gives correct errorcode
     if CameraError:
         live_answerbox_label = Label(root, text="Camera is not Connectable")
         live_answerbox_label.grid(row=5, column=3)
@@ -168,12 +182,9 @@ def ErrorEntrys(CameraError):
         live_answerbox_label = Label(root, text="Value must be Number")
         live_answerbox_label.grid(row=5, column=3)
 
+ShortInstructions = Label(root, text="ESC to close Camera window", fg="green")#fourth title
+ShortInstructions.grid(row=5, column=5)
 start_button = Button(root, text="Start Scanning", command=RecognizingStartFile)
-start_button.grid(row=6, column=5)
+start_button.grid(row=6, column=5) #at last the Start button when pressed
 
-
-
-
-
-##########################################
 root.mainloop()
--- QrRecognizingCombi.py
+++ QrRecognizingCombi.py
@@ -1,70 +1,65 @@
-import cv2
-import pyzbar.pyzbar as pyzbar
-from webcolors import *
-import pyodbc
-
-
+import cv2 #camera use and Recognize program
+import pyzbar.pyzbar as pyzbar #Recognize program
+from webcolors import * #
+import pyodbc  #database connection
 
 def defaultSettings():
-    path = 'cascade70N20S.xml'  # PATH OF THE CASCADE
-    objectName = 'QR'  # OBJECT NAME TO DISPLAY #{aanpasbaar}
-    frameWidth = 640  # DISPLAY WIDTH #{aanpasbaar}
-    frameHeight = 480  # DISPLAY HEIGHT #{aanpasbaar}
-    color = name_to_rgb('GREEN') #{aanpasbaar}
-
-    camnum = 0 #Camera nummer of IP {aanpasbaar}
+    path = 'cascade70N20S.xml'  # cascade file
+    objectName = 'QR'
+    frameWidth = 640
+    frameHeight = 480
+    color = name_to_rgb('GREEN')
+    camnum = 0 #Camera number
     return frameWidth, frameHeight, path, camnum, objectName, color
 
 
-def notneeded(a):
+def notneeded(a): #notneeded functie if neeed
     pass
 
 
-def databaseUsing(connectie, execute, insert, barcode):
-    conn = pyodbc.connect(connectie)
-    cursor = conn.cursor()
-    cursor.execute(execute)
+def databaseUsing(connectie, execute, insert, barcode): #database connnection function
+    conn = pyodbc.connect(connectie) #connection with database
+    cursor = conn.cursor() #get cursor
+    cursor.execute(execute) #execute first command
     #~
     cursor.execute('''IF NOT EXISTS (SELECT 1 FROM QRDatabase
     WHERE QRCode = ''' + str(barcode) + ''')''' +
     '''BEGIN''' + insert + '''VALUES (''' + barcode + ''') END''')
-    #~
+    #~ Execute second command
     conn.commit()
     pass
 
 
 def cascadeRunning(frameWidth, frameHeight, path, camnum, objectName, color, databaseUse, connectie, execute, insert):
-
-    cap = cv2.VideoCapture(camnum)
+    cap = cv2.VideoCapture(camnum) #activates camera
     cap.set(3, frameWidth)
     cap.set(4, frameHeight)
-    qrcodefound = False
-    cv2.namedWindow("Result")
-    cv2.resizeWindow("Result", frameWidth, frameHeight + 100)
+    qrcodefound = False  #default on False
+    cv2.namedWindow("Camera")
+    cv2.resizeWindow("Camera", frameWidth, frameHeight + 100)
     cascade = cv2.CascadeClassifier(path)
     try:
         while True:
             cap.set(10, 0) #2d value == Camera Brightness
             success, img = cap.read()
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #Make non-visable readable Camera Color
-            objects = cascade.detectMultiScale(gray, 2, 19)
+            objects = cascade.detectMultiScale(gray, 2, 19) #makes video gray to search better
             for (x, y, w, h) in objects: # Display Live Objects (Beta)
                 area = w * h
                 minArea = 20000
                 if area > minArea: #If object has correct size
                     cv2.rectangle(img, (x, y), (x + w, y + h), color, 3) #create rectangle
                     cv2.putText(img, objectName, (x, y - 5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, color, 2)
-            decodedObjects = pyzbar.decode(img)
-            for obj in decodedObjects:
-                barcode = obj.data
-                if databaseUse:
-                    databaseUsing(connectie, execute, insert, barcode)
-                print("Data", barcode)
-
+            decodedObjects = pyzbar.decode(img) #decodes barcodes out video
+            for obj in decodedObjects: #every barcodes it finds
+                barcode = obj.data #place it in a variable
+                if databaseUse: #if databaseUse is true
+                    databaseUsing(connectie, execute, insert, barcode) #use function datbase Using
+                print("Data", barcode) #print it in console
             if qrcodefound == False:
                 cv2.imshow("Result", img)
-            k = cv2.waitKey(30) & 0xff
-            if k == 27:
+            k = cv2.waitKey(30) & 0xff #wait until key pressed
+            if k == 27: #if ESC break loop
                 break
     except cv2.error:
         print("Camera not connectable")
